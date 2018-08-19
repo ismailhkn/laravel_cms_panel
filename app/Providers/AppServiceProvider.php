f9df448425db3ccf4e946e3da9eee9aec5ff7e77
ismail <ismailcikla@gmail.com>
2018-08-19 12:50:19 +0300

    A-4

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,18 +2,15 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
+
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 
     /**
